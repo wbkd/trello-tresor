3e5afd70316cfd949aed69ce745b5cff506177de
g-div <g-div@anche.no>
2018-05-25 11:54:23 +0200

    feat(cli): add meow as cli parser

--- bin/get_data.js
+++ bin/get_data.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const fs = require('fs');
 const path = require('path');
+const meow = require('meow');
 const mkdir = require('make-dir');
 const axios = require('axios');
 const nconf = require('nconf');
@@ -76,6 +77,27 @@ function getLabels(data) {
   const labelsList = [...new Set(fLabels)]; // unique
   return labelsList;
 }
+
+const cli = meow(`
+  Usage
+  $ staticms
+  Run staticms
+
+  $ staticms --help
+  Print this help message
+
+  $ staticms --version
+  Print the current version
+
+  Examples
+  $ staticms --api.key=XXX --api.token=XXX --api.list=XXX
+
+  Run the script to request the remote JSON files and save
+  them locally.
+  You can alternatively pass the configuration options as
+  environment variables or writing them to config.json.
+  See the online documentation for further information
+`);
 
 main()
   .catch(err => console.error(err.message));
