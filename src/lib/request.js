d4e4a089cab8daf8ab0d139bbe3b8465a74bb092
g-div <g-div@anche.no>
2018-05-31 16:01:09 +0200

    feat(API): allow to pass an arry of lists

--- src/lib/request.js
+++ src/lib/request.js
@@ -3,17 +3,30 @@ const axios = require('axios');
 module.exports = async config => {
   const {fields} = config;
   const {key, token, url, list} = config.api;
-  const request = {
-    method: 'get',
-    baseURL: url,
-    url: `lists/${list}/cards`,
-    params: Object.assign({}, {...fields}, {key}, {token})
-  };
 
-  try {
-    const apiResponse = await axios.request(request);
-    return apiResponse.data;
-  } catch (err) {
-    throw new Error(`Error requesting ${url}`);
+  const request = list => requests(list, url, fields, key, token);
+  if (typeof list === 'string') {
+    return request([list]);
+  } else {
+    return request(list);
   }
 };
+
+const requests = (listIds, url, fields, key, token) =>
+  listIds.reduce(async (prev, list) => {
+    const collection = await prev;
+    const req = {
+      method: 'get',
+      baseURL: url,
+      url: `lists/${list}/cards`,
+      params: Object.assign({}, {...fields}, {key}, {token})
+    };
+
+    try {
+      const apiResponse = await axios.request(req);
+      return collection.concat(apiResponse.data);
+    } catch (err) {
+      console.log(err);
+      throw new Error(`Error requesting ${url}`);
+    }
+  }, Promise.resolve([]));
